bb9724a1b5cfd29026a791c044dad82bc40cb47f
AbdeselamML9202 <98758220+AbdeselamML9202@users.noreply.github.com>
2022-02-22 19:04:38 +0100

    Added some compatibility changes to BussinesCourse.java
    Added changes that fix the compatibility eith other classes and the clone method.

--- BusinessCourse.java
+++ BusinessCourse.java
@@ -1,22 +1,33 @@
-package Bilingual_Fourth_task;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GitHubCourse;
 
+/**
+ *
+ * @author Abdeselam
+ */
 import java.time.LocalTime;
 
 public class BusinessCourse extends Course {
 
     BusinessCourse() {
-        setName("");
-        setID("");
-        setTeacher("");
-        setDuration(null);
+        setCourseName("");
+        setCourseID("");
+        setTeacherName("","");
+        setDuration(0);
     }
 
-    BusinessCourse(String name, String id, String teacher, String duration) {
-        setName(name);
-        setID(id);
-        setTeacher(teacher);
-        if (Integer.parseInt(duration) == 45) {
-            setDuration(LocalTime.of(Integer.parseInt(duration), 0, 0));
+    BusinessCourse(String name, String id, String teacher_name, String teacher_surname, String dur) {
+        setCourseName(name);
+        setCourseID(id);
+        setTeacherName(teacher_name, teacher_surname);
+        if (Integer.parseInt(dur) == 45) {
+            int entero = Integer.parseInt(dur);
+            LocalTime tiempo = LocalTime.of( entero, 0, 0); 
+            setDuration(tiempo.getHour());
         }else{
             System.err.println("Error, the duration is invalid");
         }
@@ -24,26 +35,23 @@ public class BusinessCourse extends Course {
     }
 
     BusinessCourse(BusinessCourse copy) {
-        this.setName(copy.getName());
-        this.setID(copy.getID());
+        this.setCourseName(copy.getCourseName());
+        this.setCourseID(copy.getCourseID());
         this.setDuration(copy.getDuration());
-        this.setTeacher(copy.getTeacher());
+        this.setTeacherName(copy.getTeacher(), "");
     }
 
     public String toString() {
-        return "the course" + this.getName() + "with an id" + getID() + "is with a duration" + getDuration() + "of which is taught by the teacher" + getTeacher();
+        return "the course" + this.getCourseName()+ "with an id" + getCourseID() + "is with a duration" + getDuration() + "of which is taught by the teacher" + getTeacher();
     }
 
-    public BusinessCourse clone(BusinessCourse copy) {
-        this.setName(copy.getName());
-        this.setID(copy.getID());
-        this.setDuration(copy.getDuration());
-        this.setTeacher(copy.getTeacher());
-        return copy;
+    public BusinessCourse clone() {
+        BusinessCourse clone=new BusinessCourse(this);
+        return clone;
     }
 
     public boolean equals(BusinessCourse other) {
-        if (this.getName() == other.getName() && getDuration() == other.getDuration() && getTeacher() == other.getTeacher() && this.getID() == other.getID()) {
+        if (this.getCourseName() == other.getCourseName() && getDuration() == other.getDuration() && getTeacher() == other.getTeacher() && this.getCourseID() == other.getCourseID()) {
             return true;
         }
         return false;
